ea2590121a90bda69c4e171bcb85bb8a78251c1b
Marcus Wendt <marcus@field.io>
2010-01-13 12:32:57 +0000

    adding field.kit super package object back in

---
+++ FieldKit/src.lib/field/kit/Package.scala
@@ -0,0 +1,43 @@
+/*                                                                            *\
+**           _____  __  _____  __     ____                                    **
+**          / ___/ / / /____/ / /    /    \    FieldKit                       **
+**         / ___/ /_/ /____/ / /__  /  /  /    (c) 2009, field.io             **
+**        /_/        /____/ /____/ /_____/     http://www.field.io            **
+\*                                                                            */
+/* created Jan 11, 2010 */
+package field
+
+/**
+ * Provides simplified access to the most frequently used classes and objects
+ */
+package object kit extends field.kit.math.Package {
+	
+	// -- Utilities ------------------------------------------------------------
+	type Logger = field.kit.util.Logger
+	val Logger = field.kit.util.Logger
+	
+	type Timer = field.kit.util.Timer
+	val Timer = field.kit.util.Timer
+	
+	// -- Colour ---------------------------------------------------------------
+	type Colour = field.kit.colour.Colour
+	val Colour = field.kit.colour.Colour
+	
+	// -- Processing -----------------------------------------------------------
+	type Sketch = field.kit.p5.Sketch
+	val Sketch = field.kit.p5.Sketch
+	
+	val OPENGL = processing.core.PConstants.OPENGL
+	
+	// -- Math -----------------------------------------------------------------
+	type Vec = field.kit.math.Vec
+	
+	type Vec2 = field.kit.math.Vec2
+	val Vec2 = field.kit.math.Vec2
+	
+	type Vec3 = field.kit.math.Vec3
+	val Vec3 = field.kit.math.Vec3
+	
+	type Vec4 = field.kit.math.Vec4
+	val Vec4 = field.kit.math.Vec4
+}
