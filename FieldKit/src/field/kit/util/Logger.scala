c205ce8e7545ad975e4d5962799610c0ad5ca1ed
Marcus Wendt <marcus@field.io>
2010-02-14 14:36:38 +0000

    updated logger to only generate its logName on request; saves loads of memory

--- FieldKit/src/field/kit/util/Logger.scala
+++ FieldKit/src/field/kit/util/Logger.scala
@@ -8,54 +8,54 @@
 package field.kit.util
 
 /**
- * Implements a flexible Logging System
- * @author Marcus Wendt
- */
+* Implements a flexible Logging System
+* @author Marcus Wendt
+*/
 object Logger extends Enumeration {  
-  val ALL = Value
-  val FINE = Value
-  val INFO = Value
-  val WARNING = Value
-  val ERROR = Value
-  val FATAL = Value
-  val NONE = Value
-  
-  var level = Logger.INFO
-  
-  def log(l:Value, name:String, m:Seq[Any]) {
-    if(l >= level) {
-      val s = if(l < WARNING) System.out else System.err
-    
-      val prefix = "["+ name +"]"
-      s.println( (prefix /: m) (_ +" "+ _) )
-    }
-  }
+	val ALL = Value
+	val FINE = Value
+	val INFO = Value
+	val WARNING = Value
+	val ERROR = Value
+	val FATAL = Value
+	val NONE = Value
+
+	var level = Logger.INFO
+
+	def log(l:Value, name:String, m:Seq[Any]) {
+		if(l < level) return
+		
+		val s = if(l < WARNING) System.out else System.err
+
+		val prefix = "["+ name +"]"
+		s.println( (prefix /: m) (_ +" "+ _) )
+	}
 }
 
 trait Logger {
-  private var name = className
-  
-  private def className = {
-    try {
-      var n = this.getClass.getCanonicalName
-      if(n.endsWith("$")) n = n.slice(0, n.length-1)
-      var dot = n.lastIndexOf('.')
-      if(dot < 0) n else n.substring(dot+1)
-    } catch {
-      case e:NoClassDefFoundError => "Anonymous"
-    }
-  }
-  
-  private def log(l:Logger.Value, m:Seq[Any]) = Logger.log(l, name, m)
-  
-  def logName_=(name:String) = this.name = name
-  def logName = name
-  
-  def fine(m:Any*) = log(Logger.FINE, m)
-  def info(m:Any*) = log(Logger.INFO, m)
-  def warn(m:Any*) = log(Logger.WARNING, m)
-  def error(m:Any*) = log(Logger.ERROR, m)
-  
-  def fatal(m:Any*):Unit = { log(Logger.FATAL, m); System.exit(-1) }
-  def fatal(code:Int, m:Any*) = { log(Logger.FATAL, m); System.exit(code) }
+	private var name:String = null
+
+	private def className = {
+		try {
+			var n = this.getClass.getCanonicalName
+			if(n.endsWith("$")) n = n.slice(0, n.length-1)
+			var dot = n.lastIndexOf('.')
+			if(dot < 0) n else n.substring(dot+1)
+		} catch {
+			case e:NoClassDefFoundError => "Anonymous"
+		}
+	}
+
+	private def log(l:Logger.Value, m:Seq[Any]) = Logger.log(l, name, m)
+
+	def logName_=(name:String) = this.name = name
+	def logName = if(name == null) className else name
+
+	def fine(m:Any*) = log(Logger.FINE, m)
+	def info(m:Any*) = log(Logger.INFO, m)
+	def warn(m:Any*) = log(Logger.WARNING, m)
+	def error(m:Any*) = log(Logger.ERROR, m)
+
+	def fatal(m:Any*):Unit = { log(Logger.FATAL, m); System.exit(-1) }
+	def fatal(code:Int, m:Any*) = { log(Logger.FATAL, m); System.exit(code) }
 }
